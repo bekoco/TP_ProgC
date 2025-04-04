0d1a2ea482a7391036280b1fd64a057b8f2fe103
bekoco <145817210+bekoco@users.noreply.github.com>
2025-04-04 17:02:58 +0200

    Update chaine.c

--- TP2/src/chaine.c
+++ TP2/src/chaine.c
@@ -1,42 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
     char chaine1[] = "Hello";
     char chaine2[] = "World !";
-    int Length1 = 0;
-    int Length2 = 0;
+    int len1 = 0, len2 = 0;
 
-    // Calcul de la longueur de chaine1
-    while (chaine1[Length1] != '\0') {
-        Length1++;
-    }
+    while (chaine1[len1] != '\0') len1++;
+    while (chaine2[len2] != '\0') len2++;
 
-    // Calcul de la longueur de chaine2
-    while (chaine2[Length2] != '\0') {
-        Length2++;
-    }
-
-    // Espace + 1 caractère nul → +2
-    char chaine_finale[100];
+    // +1 pour l'espace, +1 pour '\0'
+    char *chaine_finale = malloc(len1 + len2 + 2);
 
     // Copie de chaine1
-    for (int i = 0; i < Length1; i++) {
+    for (int i = 0; i < len1; i++) {
         chaine_finale[i] = chaine1[i];
     }
 
-    // Ajout de l'espace après chaine1
-    chaine_finale[Length1] = ' ';
+    // Ajout de l'espace
+    chaine_finale[len1] = ' ';
 
-    // Copie de chaine2 à la suite de l'espace
-    for (int i = 0; i < Length2; i++) {
-        chaine_finale[Length1 + 1 + i] = chaine2[i];
+    // Copie de chaine2
+    for (int i = 0; i < len2; i++) {
+        chaine_finale[len1 + 1 + i] = chaine2[i];
     }
 
-    // Ajout du caractère de fin
-    chaine_finale[Length1 + 1 + Length2] = '\0';
+    // Terminaison de la chaîne
+    chaine_finale[len1 + 1 + len2] = '\0';
 
-    // Affichage de la chaîne finale
+    // Affichage
     printf("%s\n", chaine_finale);
 
+    // Libération mémoire
+    free(chaine_finale);
     return 0;
 }

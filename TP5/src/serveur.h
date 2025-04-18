d34600de347b486e6a556c9ddd6a6afa6abd6c9e
bekoco <145817210+bekoco@users.noreply.github.com>
2025-04-18 15:17:00 +0200

    Update serveur.h

--- TP5/src/serveur.h
+++ TP5/src/serveur.h
@@ -10,35 +10,10 @@
 
 #define PORT 8089
 
-/**
- * Envoie un message au client via le socket donné.
- * @param client_socket_fd : socket du client
- * @param sdata : message à envoyer
- * @return EXIT_SUCCESS ou EXIT_FAILURE
+/* accepter la nouvelle connection d'un client et lire les données
+ * envoyées par le client. En suite, le serveur envoie un message
+ * en retour
  */
-int renvoie_message(int client_socket_fd, char *sdata);
-
-/**
- * Reçoit un message du client, affiche ce message,
- * demande une réponse à l'utilisateur serveur, et la renvoie au client.
- * @param client_socket_fd : socket du client
- * @param data : message reçu
- * @return EXIT_SUCCESS ou EXIT_FAILURE
- */
-int recois_envoie_message(int client_socket_fd, char *data);
-
-/**
- * Gère la session complète avec un client :
- * réception, réponse, boucle, et déconnexion sur "exit"
- * @param client_socket_fd : socket du client
- */
-void gerer_client(int client_socket_fd);
-
-/**
- * Gère proprement le signal Ctrl+C (SIGINT)
- * pour fermer le socket serveur avant de quitter.
- * @param signal : le signal capturé
- */
-void gestionnaire_ctrl_c(int signal);
+int renvoie_message(int, char *);
 
 #endif
